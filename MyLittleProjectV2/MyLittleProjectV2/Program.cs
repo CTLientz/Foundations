6d853e51370eefed79435e96deffda1ccc899fd1
Chelsea L <Chelsea@yertz.com>
2016-10-06 19:13:33 -0700

    My first C# projects

---
+++ MyLittleProjectV2/MyLittleProjectV2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace MyLittleProjectV2
+{
+   
+    class MyLittleProjectV2
+    {
+        static void Main()
+
+        {
+            Console.WriteLine("Andy");
+            Console.WriteLine("I like the fill option.");
+            Console.WriteLine("But I feel like I should be able to tab through it.");
+            Console.WriteLine("ok, that was awesome.  I tried tabbing and it worked.");
+            Console.WriteLine("Maybe this coding thing won't be so bad.");
+            Console.WriteLine("Crap.  I forgot to write ReadLine");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Session2-CTL/Session2-CTL/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class MyLittleProject
+{
+    static void Main() //My Entry to the program
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Are you hungry?");
+        Console.ReadLine();
+    }
+}//END OF MY CLASS
