1de00c42481833ad6cd54c5fb6d1d973387b111d
Chelsea L <Chelsea@yertz.com>
2016-10-27 17:54:51 -0700

    27102016

--- Homework_FractoralOfInteger_27102016/Homework_FractoralOfInteger_27102016/Program.cs
+++ Homework_FractoralOfInteger_27102016/Homework_FractoralOfInteger_27102016/Program.cs
@@ -8,8 +8,32 @@ namespace Homework_FractoralOfInteger_27102016
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+
+            Console.WriteLine("Let's find the fractoral of your favorite number.");
+            Console.WriteLine("Enter a number:");
+
+            string str = Console.ReadLine();
+
+            int nNumb = int.Parse(str);
+            int x;
+
+            int nFact = 1;
+
+
+            for (x = 1; x < nNumb; x++)
+            {
+                nFact = nFact * x;
+            }
+          
+
+            Console.WriteLine("POOF!  The Fractoral of {0} is {1}.", nNumb, nFact);
+
+
+            Console.ReadLine();
+
+            //This is still a complicated problem for me.  I
         }
     }
 }
